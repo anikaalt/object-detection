e3e5562c53f0bd39153de5f89e8f08aedb63bb45
anikaalt <89435681+anikaalt@users.noreply.github.com>
2022-09-16 19:57:58 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+/*
+img = "";
+
+function preload()
+{
+    img = loadImage("dog_cat.jpg");
+}
+
+function setup()
+{
+    canvas = createCanvas(640,420);
+    canvas.center();
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+    fill("#BCA481");
+    text("dog", 45, 75);
+    noFill();
+    stroke("#BCA481");
+    rect(30, 60, 450, 350);
+}
+*/
